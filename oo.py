7133c2ef023b5a81a20f3b1cf28c5eb3f099b252
Alison Cordeiro <acordeiro@gmail.com>
2021-02-20 19:15:36 -0800

    Update Library and Square functions

--- oo.py
+++ oo.py
@@ -4,14 +4,15 @@
    can provide? Explain each concept.
 
 Abstraction allows us to consolidate shared attributes or behaviors into parent
- classes, and to use methods even if we don't know quite how they work.
+    classes, and to use methods even without knowing quite how they work.
 Encapsulation means data is well-contained and lives close to its functionality. 
-Polymorphism: selective inheritance keeps code adaptive, flexible, and dry.
+Polymorphism: selective inheritance allows for interchangeable functionality.
+    This keeps code adaptive, flexible, and dry.
 
 
 2. What is a class?
 
-A class is a group that shares structure, attributes, and/or behaviors. 
+A class is a group that can share structure, attributes, and/or behaviors. 
 
 
 3. What is a method?
@@ -22,6 +23,7 @@ A method is a function that applies to a class.
 4. What is an instance in object orientation?
 
 An instance is a specific occurrence of a given class.
+
 
 5. How is a class attribute different than an instance attribute?
    Give an example of when you might use each.
@@ -83,13 +85,43 @@ class Book(object):
 
         self.title = title
         self.author = author
-        self.books = []
+
+    def __repr__(self):
+        return f"{self.title} by {self.author}"
     
+
+class Library:
+    """A Library object."""
+
+    def __init__(self):
+        """Create a library."""
+
+        self.books = []
+
     def create_and_add_book(self, title, author):
-        pass
+        """Instantiate a new Book object and add to the library's book list."""
+
+        new_book = Book(title, author)
+        self.books.append(new_book)
 
     def find_books_by_author(self, author):
-        pass
+        """Search by author, returning a list of books by that author.
+        
+        Note: Assignment language is vague, such that "books" by the author
+        can be interpreted as "book titles" or "Book objects." Because "book" 
+        was not capitalized, I have chosen to interpret it as the book title.
+
+        However, to return a list of objects instead, line 122 should read: 
+                books_by_author.append(book)
+        """
+
+        books_by_author = []
+
+        for book in self.books:
+            if author == book.author:
+                books_by_author.append(book.title)
+        
+        return books_by_author
 
 
 """5. Rectangle"""
@@ -108,3 +140,22 @@ class Rectangle:
         """Return the area of the rectangle."""
 
         return self.length * self.width
+
+
+class Square(Rectangle):
+    """A Square type of Rectangle."""
+
+    def __init__(self, side):
+        """Create a square with identical length and width."""
+
+        super().__init__(side, side)
+    
+    def calculate_area(self):
+        """Return the area if the Square passes validation."""
+
+        if self.length == self.width:
+            return super().calculate_area()
+        else:
+            print("Invalid square")
+            return None
+
